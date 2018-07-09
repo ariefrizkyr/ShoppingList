890719f12bfb1d2411f02e97a3e399339346255b
Arief R Ramadhan <ariefrizkyr@gmail.com>
2018-07-09 16:47:09 +0700

    Updated database

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: 'mongodb://ariefrizkyr:Omgoogle1234!@ds129821.mlab.com:29821/arief_shopping_list'
-}
+  mongoURI: 'mongodb://4baed419b99e14a0baaef4d89388df70:efb2928a3e0ec5b98a1694ceee444adc@leader.mongodb.discoverd:27017/32ee6458569c9b46d64bc82190485856'
+};
